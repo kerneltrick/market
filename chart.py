be00bf8144ba49bd9d2e9f421e8b18f0d9c76a2e
Mark <markjimenez3@gmail.com>
2021-01-25 20:53:20 -1000

    feat: started building chart class

--- chart.py
+++ chart.py
@@ -1,5 +1,14 @@
 import numpy as np
 import os
+from datetime import datetime, timedelta
 
 
 class Chart:
+
+    def __init__(self, symbol, startDate, endDate):
+        self.symbol = symbol
+        self.begin = datetime.strptime(startDate, '%Y-%m-%d')
+        self.end = datetime.strptime(startDate, '%Y-%m-%d')
+        
+ 
+
